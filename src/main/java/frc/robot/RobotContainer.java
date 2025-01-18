1f8ed167b2a33a13bb68fa0523340c3362d71642
LAUNCH TEAM 6352 <frc6352@gmail.com>
2025-01-18 15:34:25 -0700

    Implement Drive Train functionality, begin test system, set up robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,22 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import java.util.Optional;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.DriveWithGamepad;
+import frc.robot.commands.test.TestDriveTrain;
+import frc.robot.subsystems.DriveTrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -20,18 +29,91 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer
 {
-    // The robot's subsystems and commands are defined here...
-    private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    // Subsystems:
+    private final Optional<DriveTrain> driveTrain;
 
-    // Replace with CommandPS4Controller or CommandJoystick if needed
-    private final CommandXboxController m_driverController = new CommandXboxController(
-        OperatorConstants.kDriverControllerPort);
+   // OI devices:
+    private final XboxController driverGamepad;
+    private final XboxController codriverGamepad;
+
+    SendableChooser<Boolean> driveOrientationChooser = new SendableChooser<>();
+    SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+    
+
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer()
     {
-        // Configure the trigger bindings
-        configureBindings();
+         // Get the game data message fom the driver station.
+        // This message is primarily used during development to
+        // construct only certain subsystems.
+        // If the message is blank (or all whitespace),
+        // all subsystems are constructed.
+        // Otherwise, OI devices and subsystems are constructed
+        // depending upon the substrings found in the message:
+        // -dt- Drive train
+        
+        var gameData = DriverStation.getGameSpecificMessage().toLowerCase();
+        SmartDashboard.putString("Game Data", gameData);
+
+        
+        // Create OI devices:
+        if (gameData.contains("-oi-"))
+        {
+            // Explicitly look for OI devices:
+            driverGamepad = DriverStation.isJoystickConnected(OperatorConstants.driverGamepadPort)
+                ? new XboxController(OperatorConstants.driverGamepadPort)
+                : null;
+            codriverGamepad = DriverStation.isJoystickConnected(OperatorConstants.codriverGamepadPort)
+                ? new XboxController(OperatorConstants.codriverGamepadPort)
+                : null;
+        }
+        else
+        {
+            // In competition, don't take chances and always create all OI devices:
+            codriverGamepad = new XboxController(OperatorConstants.codriverGamepadPort);
+            driverGamepad = new XboxController(OperatorConstants.driverGamepadPort);
+        }
+
+         // Create subsystems:
+         driveTrain = gameData.isBlank() || gameData.contains("-dt-")
+             ? Optional.of(new DriveTrain())
+             : Optional.empty();
+
+         // Configure commands for Path Planner:
+         configurePathPlannerNamedCommands();
+
+         // Configure default commands
+         configureDefaultCommands();
+ 
+         // Configure the trigger bindings
+         configureBindings();
+ 
+         // Configure smart dashboard
+         configureSmartDashboard();
+    }
+
+    /**
+     * Configure named commands for Path Planner.
+     */
+    private void configurePathPlannerNamedCommands()
+    {
+       // EX:  NamedCommands.registerCommand("Shoot Wait", new Wait(AutoKeys.shootWaitTime));
+    }
+
+    /**
+     * Configures the default commands.
+     */
+    private void configureDefaultCommands()
+    {
+        driveTrain.ifPresent((dt) ->
+        {
+            if (driverGamepad != null)
+            {
+                dt.setDefaultCommand(new DriveWithGamepad(dt, driverGamepad, driveOrientationChooser));
+            }
+        });
     }
 
     /**
@@ -46,12 +128,31 @@ public class RobotContainer
     private void configureBindings()
     {
         // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-        new Trigger(m_exampleSubsystem::exampleCondition)
-            .onTrue(new ExampleCommand(m_exampleSubsystem));
+
 
         // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
         // cancelling on release.
-        m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    }
+
+    private void configureSmartDashboard()
+    {
+        driveTrain.ifPresent(this::configureSmartDashboard);
+
+        // Configure chooser widgets:
+        configureDriveOrientationChooser(driveOrientationChooser);
+
+    }
+
+    private void configureSmartDashboard(DriveTrain driveTrain)
+    {
+    }
+
+    private void configureDriveOrientationChooser(SendableChooser<Boolean> driveOrientationChooser)
+    {
+        driveOrientationChooser.setDefaultOption("Field Relative", Boolean.TRUE);
+        driveOrientationChooser.addOption("Robot Relative", Boolean.FALSE);
+        SmartDashboard.putData("Drive Orientation", driveOrientationChooser);
     }
 
     /**
@@ -62,6 +163,21 @@ public class RobotContainer
     public Command getAutonomousCommand()
     {
         // An example command will be run in autonomous
-        return Autos.exampleAuto(m_exampleSubsystem);
+        return null;
+    }
+
+     /**
+     * Builds a command to run when Test mode is enabled in the Driver Station.
+     */
+    public Command getTestCommand()
+    {
+        var group = new SequentialCommandGroup();
+
+        if (driveTrain.isPresent())
+        {
+            group.addCommands(new TestDriveTrain(driveTrain.get()));
+        }
+
+        return group;
     }
 }
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,150 @@
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.PathPlannerConstants;
+import frc.robot.Constants.SwerveConstants;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+
+public class DriveTrain extends SubsystemBase 
+{
+    public SwerveDrive swerveDrive;
+
+    public DriveTrain()
+    {
+        // Swerve drive initiation according to YAGSL
+        try
+        {
+            SwerveDriveTelemetry.verbosity = DriveConstants.swerveDriveTelemetryVerbosity;
+            File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+           
+            swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(SwerveConstants.maxModuleSpeedMps);
+
+            SmartDashboard.putNumber("swerve/baseRadius", swerveDrive.swerveDriveConfiguration.getDriveBaseRadiusMeters());
+
+            setupPathPlanner();
+
+        } catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative)
+    {
+        // Open loop is disabled since it shouldn't be used most of the time.
+        swerveDrive.drive(translation, rotation, fieldRelative, false);
+    }
+
+    // Drive the robot given a chassis field oriented velocity.
+    public void driveFieldOriented(ChassisSpeeds velocity)
+    {
+        swerveDrive.driveFieldOriented(velocity);
+    }
+
+    // Drive according to the chassis robot oriented velocity.
+    public void drive(ChassisSpeeds velocity)
+    {
+        swerveDrive.drive(velocity);
+    }
+
+    public void setupPathPlanner()
+    {
+        RobotConfig config = null;
+        try
+        {
+            config = RobotConfig.fromGUISettings();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting
+                                    // pose)
+            this::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedForwards) -> setChassisSpeeds(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            new PPHolonomicDriveController(
+                // Translation PID constants
+                PathPlannerConstants.TRANSLATION_PID,
+                // Rotation PID constants
+                PathPlannerConstants.ANGLE_PID
+            ),
+            config,
+            () ->
+            {
+                // Boolean supplier that controls when the path will be mirrored for the red
+                // alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                var alliance = DriverStation.getAlliance();
+                return alliance.isPresent() ? alliance.get() == DriverStation.Alliance.Red : false;
+            },
+            this // Reference to this subsystem to set requirements
+        );
+    }
+
+    /**
+     * Gets the current pose (position and rotation) of the robot, as reported by
+     * odometry.
+     *
+     * @return The robot's pose
+     */
+    public Pose2d getPose()
+    {
+        return swerveDrive.getPose();
+    }
+
+    /**
+     * Resets odometry to the given pose. Gyro angle and module positions do not
+     * need to be reset when calling this
+     * method. However, if either gyro angle or module position is reset, this must
+     * be called in order for odometry to
+     * keep working.
+     *
+     * @param initialHolonomicPose
+     *            The pose to set the odometry to
+     */
+    public void resetOdometry(Pose2d initialHolonomicPose)
+    {
+        swerveDrive.resetOdometry(initialHolonomicPose);
+    }
+
+    /**
+     * Gets the current velocity (x, y and omega) of the robot
+     *
+     * @return A {@link ChassisSpeeds} object of the current velocity
+     */
+    public ChassisSpeeds getRobotVelocity()
+    {
+        return swerveDrive.getRobotVelocity();
+    }
+
+    /**
+     * Set chassis speeds with closed-loop velocity control.
+     *
+     * @param chassisSpeeds
+     *            Chassis Speeds to set.
+     */
+    public void setChassisSpeeds(ChassisSpeeds chassisSpeeds)
+    {
+        swerveDrive.setChassisSpeeds(chassisSpeeds);
+    }
+}
