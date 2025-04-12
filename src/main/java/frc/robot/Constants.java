6a882dbb737645cc3d273ce88c51c15344f543a5
LAUNCH TEAM <frc6352@gmail.com>
2025-04-11 19:58:38 -0700

    Slightly tweak elevator constants based on driver practice on 4/8 and 4/11

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -203,13 +203,13 @@ public final class Constants
 
         public static final class PIDConstants
         {
-            public static final double kP = 0.1; // Elevator slams down if increased
+            public static final double kP = 0.13; // Elevator lowers safely at this speed
             public static final double kI = 0.0;
             public static final double kD = 0.0;
             public static final int kIZ = 0;
             public static final double kFF = 0;
-            public static final double minOutput = -0.5;
-            public static final double maxOutput = 0.7;
+            public static final double minOutput = -0.4;
+            public static final double maxOutput = 0.5;
             public static final double tolerance = 0.5;
         }
     }
